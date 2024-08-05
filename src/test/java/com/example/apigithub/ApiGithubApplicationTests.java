5c02d273b3ef36bc7b45fcd3c74caa4c403d2f52
Daniel Nagorka <danielosnag1996@gmail.com>
2024-08-05 19:08:11 +0200

    First Commit

---
+++ src/test/java/com/example/apigithub/ApiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
